4db16c4688380b34d0f73c3a0f2da61db0e63840
lucenstuff <lucentiniagustin@hotmail.com>
2025-05-12 19:49:45 -0300

    Implement AsignacionDocenteServiceImp class to handle AsignacionDocente operations

---
+++ src/main/java/com/imb2025/calificaciones/service/AsignacionDocenteServiceImp.java
@@ -0,0 +1,37 @@
+package com.imb2025.calificaciones.service;
+
+import com.imb2025.calificaciones.entity.AsignacionDocente;
+import com.imb2025.calificaciones.repository.AsignacionDocenteRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AsignacionDocenteServiceImp implements AsignacionDocenteService {
+
+    @Autowired
+    private AsignacionDocenteRepository repository;
+
+    public List<AsignacionDocente> findAll() {
+        return repository.findAll();
+    }
+
+    public AsignacionDocente findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public AsignacionDocente save(AsignacionDocente asignacionDocente) {
+        return repository.save(asignacionDocente);
+    }
+
+    public AsignacionDocente update(Long id, AsignacionDocente asignacionDocente) {
+        asignacionDocente.setId(id);
+        return repository.save(asignacionDocente);
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+}
