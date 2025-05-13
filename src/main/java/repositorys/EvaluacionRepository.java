4fade32e2dcd07e4d4de76624b396dbf5edeea56
AgusMarconi <agustinamarconi39@gmail.com>
2025-05-13 00:31:41 -0300

    Create and configure EvaluacionRepository

---
+++ src/main/java/repositorys/EvaluacionRepository.java
@@ -0,0 +1,10 @@
+package repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import entitys.Evaluacion;
+
+public interface EvaluacionRepository extends JpaRepository<Evaluacion, Integer> {
+
+	
+}
