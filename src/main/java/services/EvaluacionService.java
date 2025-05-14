71fa3fab997e8631133d56aa4b7b9064146d1025
AgusMarconi <agustinamarconi39@gmail.com>
2025-05-13 22:38:51 -0300

    Feature: create methods abstract JPA

--- src/main/java/services/EvaluacionService.java
+++ src/main/java/services/EvaluacionService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 public interface EvaluacionService {
 
-	List<Evaluacion> findAll();
-	Evaluacion findById(int id);
-	
+	List<Evaluacion> findAll(); //Todos las evaluaciones GET ALL
+	Evaluacion findById(int id); //Encontrar una Evaluacion por id GET ID
+	Evaluacion save(Evaluacion evaluacion); //Crear y guardar Evaluacion POST
+	Evaluacion update(int id, Evaluacion evaluacion); //Actualizar Evaluacion PUT
+	void delete(int id);  //Eliminar Evaluacion
 	
 }
