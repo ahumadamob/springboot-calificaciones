158d5b9ba80c8e605b8e7131f2f3703c6123c248
stefano <stefanomejiasuni390@gmail.com>
2025-05-15 16:58:18 -0300

    TP01-"TipoNota"

---
+++ src/main/java/com/imb2025/calificaciones/entity/TipoNota.java
@@ -0,0 +1,52 @@
+package com.imb2025.calificaciones.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tipo_nota")
+public class TipoNota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String descripcion;
+
+    
+    public TipoNota() {}
+
+    public TipoNota(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    // Getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
