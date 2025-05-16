352cbe5f472380d068e54d94abbed9e7e85119ad
AgusMarconi <agustinamarconi39@gmail.com>
2025-05-15 23:15:50 -0300

    arreglando @overrides

--- src/main/java/com/imb2025/calificaciones/services/EvaluacionServiceImp.java
+++ src/main/java/com/imb2025/calificaciones/services/EvaluacionServiceImp.java
@@ -9,29 +9,34 @@ import com.imb2025.calificaciones.repositories.EvaluacionRepository;
 import java.util.List;
 
 @Service
-public class EvaluacionServiceImp {
+public class EvaluacionServiceImp implements EvaluacionService{
 	
 	@Autowired
 	private EvaluacionRepository evaluacionRepository;
 	
+	@Override
 	public List<Evaluacion> findAll(){
 		return evaluacionRepository.findAll();
 	}
 	
+	@Override
 	public Evaluacion findById(int id) {
 		return evaluacionRepository.findById(id).orElse(null);
 	}
 	
+	@Override
 	public Evaluacion save(Evaluacion evalucion) {
 		return evaluacionRepository.save(evalucion);
 	}
 	
+	@Override
 	public Evaluacion update(int id, Evaluacion evaluacion) {
 		evaluacion.setId(id);
 		return evaluacionRepository.save(evaluacion);
 	
 	}
 	
+	@Override
 	public void deleteById(int id) {
 		evaluacionRepository.deleteById(id);
 	}
