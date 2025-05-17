9fd1eb5caec897f0db88498398ca548165f4d84f
Tomás Sosa <tsosa416@gmail.com>
2025-05-17 20:12:02 -0300

    Renombrada interfaz y movida implementación al paquete service.jpa

---
+++ src/main/java/com/imb2025/calificaciones/condicionfinal/service/jpa/CondicionFinalServiceImpl.java
@@ -0,0 +1,33 @@
+package com.imb2025.calificaciones.condicionfinal.service.jpa;
+
+import com.imb2025.calificaciones.condicionfinal.entity.CondicionFinal;
+import com.imb2025.calificaciones.condicionfinal.repository.CondicionFinalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CondicionFinalServiceImpl implements ICondicionFinalService {
+
+    @Autowired
+    private CondicionFinalRepository repository;
+
+    public List<CondicionFinal> getAll() {
+        return repository.findAll();
+    }
+
+    public CondicionFinal save(CondicionFinal cf) {
+        return repository.save(cf);
+    }
+
+    public CondicionFinal getById(Long id) {
+        Optional<CondicionFinal> result = repository.findById(id);
+        return result.orElse(null);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
