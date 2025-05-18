8e6905139f6e610c90b58376c525aa9ad492896b
Agustin <pradoagustin505@gmail.com>
2025-05-17 23:31:22 -0300

    tp01-prado-agustin-entidad materia

---
+++ src/main/java/com/imb2025/calificaciones/Repository/MateriaRepository.java
@@ -0,0 +1,13 @@
+package com.imb2025.calificaciones.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.imb2025.calificaciones.entity.Materia;
+
+
+
+public interface MateriaRepository extends JpaRepository<Materia,Long>{
+	
+
+}
+
---
+++ src/main/java/com/imb2025/calificaciones/serviceJpa/MateriaServiceImpl.java
@@ -0,0 +1,44 @@
+package com.imb2025.calificaciones.serviceJpa;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.imb2025.calificaciones.Repository.MateriaRepository;
+import com.imb2025.calificaciones.entity.Materia;
+import com.imb2025.calificaciones.service.IMateriaService;
+
+
+@Service
+public class MateriaServiceImpl implements IMateriaService{
+	
+	@Autowired
+	private MateriaRepository repo;
+
+	@Override
+	public List<Materia> findAll() {
+		
+		return repo.findAll();
+		
+	}
+
+	@Override
+	public Materia findById(Long id) {
+	
+		
+		
+		return repo.findById(id).orElse(null);
+	}
+
+	@Override
+	public Materia save(Materia materia) {
+		
+		return repo.save(materia);
+	}
+
+	@Override
+	public void deleteById(long id) {
+		repo.deleteById(id);
+		
+	}
+}
