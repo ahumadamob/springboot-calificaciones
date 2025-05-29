3b83bc4a31b3e285714061bdda009707046c10f5
Federico Ordenes <53576512+ISlidexI@users.noreply.github.com>
2025-05-28 22:26:17 -0300

    tp 01 programacion (#20)
    * tp 01 programacion
    
    * cambien la o con acento sin asento

---
+++ src/main/java/com/imb2025/calificaciones/repository/ComisionRepository.java
@@ -0,0 +1,10 @@
+package com.imb2025.calificaciones.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.imb2025.calificaciones.entity.Comision;
+
+public interface ComisionRepository extends JpaRepository<Comision
+, Long>{
+
+}
