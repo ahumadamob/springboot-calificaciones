77a40e7b858d4203ad967b44df176a4a18078ece
Mario Ahumada <ahumadamob@gmail.com>
2025-06-14 14:12:21 -0300

    Correcciones del profesor

--- src/main/java/com/imb2025/calificaciones/condicionfinal/repository/CondicionFinalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.imb2025.calificaciones.condicionfinal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.imb2025.calificaciones.condicionfinal.entity.CondicionFinal;
-
-public interface CondicionFinalRepository extends JpaRepository<CondicionFinal, Long> {
-}
--- src/main/java/com/imb2025/calificaciones/service/IAlumnoServices.java
+++ src/main/java/com/imb2025/calificaciones/service/IAlumnoServices.java
@@ -3,7 +3,6 @@ package com.imb2025.calificaciones.service;
 import com.imb2025.calificaciones.entity.Alumno;
 
 import java.util.List;
-import java.util.Optional;
 
 
 public interface IAlumnoServices {
