a85d26ec7bcba29c89513b064a93b00321957539
unknown <kianzo.web@gmail.com>
2025-06-26 19:07:52 -0300

    TP02 - Relaciones y DTO implementados

--- src/main/java/com/imb2025/calificaciones/entity/Cursada.java
+++ src/main/java/com/imb2025/calificaciones/entity/Cursada.java
@@ -5,49 +5,62 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import jakarta.persistence.*;
+
 @Entity
 public class Cursada {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id; 
-	private Long alumnoId;
-	private Long materiaId;
-	private Long anioLectivoId;
-	private Long condicionFinalId;
-	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Long getAlumnoId() {
-		return alumnoId;
-	}
-	public void setAlumnoId(Long alumnoId) {
-		this.alumnoId = alumnoId;
-	}
-	public Long getMateriaId() {
-		return materiaId;
-	}
-	public void setMateriaId(Long materiaId) {
-		this.materiaId = materiaId;
-	}
-	public Long getAnioLectivoId() {
-		return anioLectivoId;
-	}
-	public void setAnioLectivoId(Long anioLectivoId) {
-		this.anioLectivoId = anioLectivoId;
-	}
-	public Long getCondicionFinalId() {
-		return condicionFinalId;
-	}
-	public void setCondicionFinalId(Long condicionFinalId) {
-		this.condicionFinalId = condicionFinalId;
-	}
-	
-	
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Relación con Alumno: muchos cursadas pueden tener un mismo alumno
+    @ManyToOne
+    @JoinColumn(name = "alumno_id")
+    private Alumno alumno;
+
+    // Relación con Materia: muchos cursadas pueden tener una misma materia
+    @ManyToOne
+    @JoinColumn(name = "materia_id")
+    private Materia materia;
+
+    private String anioLectivo;
+
+    // Relación con CondicionFinal: muchos cursadas pueden tener una misma condición
+    @ManyToOne
+    @JoinColumn(name = "condicion_final_id")
+    private CondicionFinal condicionFinal;
+
+  
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public String getAnioLectivo() {
+        return anioLectivo;
+    }
+
+    public void setAnioLectivo(String anioLectivo) {
+        this.anioLectivo = anioLectivo;
+    }
+
+    public CondicionFinal getCondicionFinal() {
+        return condicionFinal;
+    }
+
+    public void setCondicionFinal(CondicionFinal condicionFinal) {
+        this.condicionFinal = condicionFinal;
+    }
 }
