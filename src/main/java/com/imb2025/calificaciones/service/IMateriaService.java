7a44587621306973d1de201812e36cf65c9a0d4d
Agustin Prado <143540840+PradoAgustin-cmd@users.noreply.github.com>
2025-06-26 12:17:22 -0300

    Tp2 prado agustin (#45)
    * TP02 - Relaciones y DTO implementados
    
    * todos los acambios agregados

--- src/main/java/com/imb2025/calificaciones/service/IMateriaService.java
+++ src/main/java/com/imb2025/calificaciones/service/IMateriaService.java
@@ -2,6 +2,7 @@ package com.imb2025.calificaciones.service;
 
 import java.util.List;
 
+import com.imb2025.calificaciones.dto.MateriaRequestDTO;
 import com.imb2025.calificaciones.entity.Materia;
 
 
@@ -12,8 +13,11 @@ public interface IMateriaService {
 
 	public Materia findById(Long id);
 
-	public Materia save(Materia materia);
+	public Materia create(Materia materia);
 
 	public void deleteById(long id);
+	
+	public Materia update(Materia materia, Long id) throws Exception;
+	public Materia mapFromDto(MateriaRequestDTO materiaRequestDto) throws Exception;
 
 }
