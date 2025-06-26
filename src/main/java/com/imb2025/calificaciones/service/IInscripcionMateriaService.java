a248baa0d43326500280585f7530e7ced16958ac
Victor Nievas <79821210+VictorNievas96@users.noreply.github.com>
2025-06-26 19:29:12 -0300

    Tp02 nievas victor (#47)
    * TP02
    
    * Validaciones de errores

--- src/main/java/com/imb2025/calificaciones/service/IInscripcionMateriaService.java
+++ src/main/java/com/imb2025/calificaciones/service/IInscripcionMateriaService.java
@@ -2,12 +2,14 @@ package com.imb2025.calificaciones.service;
 
 import java.util.List;
 
+import com.imb2025.calificaciones.dto.InscripcionMateriaRequestDTO;
 import com.imb2025.calificaciones.entity.InscripcionMateria;
 
 public interface IInscripcionMateriaService {
     InscripcionMateria findById(Long id);
     List<InscripcionMateria> findAll();
-    InscripcionMateria save(InscripcionMateria inscripcionMateria);
-    InscripcionMateria update(Long id, InscripcionMateria inscripcionMateria);
+    InscripcionMateria create(InscripcionMateria inscripcionMateria);
+    InscripcionMateria update(Long id,InscripcionMateria inscripcionMateria) throws Exception;
     void deleteById(Long id);
+    InscripcionMateria mapFromDto(InscripcionMateriaRequestDTO inscripcionMateriaDTO) throws Exception;
 }
