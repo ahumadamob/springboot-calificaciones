e7b4c046305248d8871075e63d78b36753f81fa7
Susana Viertel Corona <olgaviertel@gmail.com>
2025-06-29 18:14:16 -0300

    Limpieza: elimino PeriodoLectivo para reiniciar TP02 desde cero

--- src/main/java/com/imb2025/calificaciones/condicionfinal/exception/EntidadNoEncontradaException.java
+++
@@ -1,7 +0,0 @@
-package com.imb2025.calificaciones.condicionfinal.exception;
-
-public class EntidadNoEncontradaException extends RuntimeException {
-    public EntidadNoEncontradaException(String mensaje) {
-        super(mensaje);
-    }
-}
