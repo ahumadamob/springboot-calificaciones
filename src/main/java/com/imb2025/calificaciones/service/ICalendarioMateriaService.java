5c7b4029f70359c274234e02de68106a0d0f61ec
Gonzalo Infante <126709813+Infgonza@users.noreply.github.com>
2025-06-29 14:11:50 -0300

    TP02 - Relaciones y DTO implementados (#37)
    * TP02 - Relaciones y DTO implementados
    
    * Relaciones y DTO

--- src/main/java/com/imb2025/calificaciones/service/ICalendarioMateriaService.java
+++ src/main/java/com/imb2025/calificaciones/service/ICalendarioMateriaService.java
@@ -2,14 +2,17 @@ package com.imb2025.calificaciones.service;
 
 import java.util.List;
 
+import com.imb2025.calificaciones.dto.CalendarioMateriaRequestDTO;
 import com.imb2025.calificaciones.entity.CalendarioMateria;
 
 public interface ICalendarioMateriaService {
 	
 	List<CalendarioMateria> findAll();
 	CalendarioMateria findByID(Long id);
-	CalendarioMateria save(CalendarioMateria calendarioMateria);
-	CalendarioMateria update(Long id, CalendarioMateria calendarioMateria);
+	CalendarioMateria create(CalendarioMateria calendarioMateria);
+	CalendarioMateria update(Long id, CalendarioMateria calendarioMateria) throws Exception;
 	void delete(Long id);
-	
+	CalendarioMateria mapFromDto(CalendarioMateriaRequestDTO calMatDto)throws Exception;
+
+
 }
--- src/main/java/com/imb2025/calificaciones/service/jpa/CalendarioMateriaServiceImpl.java
+++ src/main/java/com/imb2025/calificaciones/service/jpa/CalendarioMateriaServiceImpl.java
@@ -1,6 +1,15 @@
 package com.imb2025.calificaciones.service.jpa;
 
 import java.util.List;
+import java.util.Optional;
+
+import com.imb2025.calificaciones.dto.CalendarioMateriaRequestDTO;
+import com.imb2025.calificaciones.entity.Comision;
+import com.imb2025.calificaciones.entity.Materia;
+import com.imb2025.calificaciones.repository.ComisionRepository;
+import com.imb2025.calificaciones.repository.MateriaRepository;
+import jakarta.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +24,11 @@ public class CalendarioMateriaServiceImpl implements ICalendarioMateriaService{
 
 	@Autowired
 	private ICalendarioMateriaRepository calMatRepo;
-	
+	@Autowired
+	private MateriaRepository materiaRepository;
+	@Autowired
+	private ComisionRepository comisionRepository;
+
 	@Override
 	public List<CalendarioMateria> findAll() {
 		return calMatRepo.findAll();
@@ -28,16 +41,19 @@ public class CalendarioMateriaServiceImpl implements ICalendarioMateriaService{
 	}
 
 	@Override
-	@Transactional
-	public CalendarioMateria save(CalendarioMateria calendarioMateria) {
+	public CalendarioMateria create(CalendarioMateria calendarioMateria) {
 		return calMatRepo.save(calendarioMateria);
 	}
-	
+
 	@Override
-	@Transactional
-	public CalendarioMateria update(Long id, CalendarioMateria calendarioMateria) {
-		calendarioMateria.setId(id);
-		return calMatRepo.save(calendarioMateria);
+	public CalendarioMateria update(Long id, CalendarioMateria calendarioMateria) throws Exception {
+
+		if (calMatRepo.existsById(id)){
+			calendarioMateria.setId(id);
+			return calMatRepo.save(calendarioMateria);
+		} else {
+			throw new Exception ("No se encontro calendario materia con el id: "+ id);
+		}
 	}
 
 	@Override
@@ -47,9 +63,30 @@ public class CalendarioMateriaServiceImpl implements ICalendarioMateriaService{
 		if (calMatRepo.existsById(id)){
 			calMatRepo.deleteById(id);
 		}
-	
+
 	}
 
+	@Override
+	public CalendarioMateria mapFromDto(CalendarioMateriaRequestDTO calMatDto) throws Exception {
+
+		CalendarioMateria calendarioMateria = new CalendarioMateria();
+
+		if (calMatDto.getFechaInicio().isAfter(calMatDto.getFechaFin())){
+			throw new Exception("La fecha de inicio no puede ser posterior a la fecha de fin");
+		}
+		Materia materia = materiaRepository.findById(calMatDto.getMateriaId())
+				.orElseThrow(() -> new Exception ("Materia no encontrada con el id: " + calMatDto.getMateriaId()));
+
+		Comision comision = comisionRepository.findById(calMatDto.getComisionId())
+				.orElseThrow(() -> new Exception ("Comision no encontrada con el id: " + calMatDto.getComisionId()));
+
+		calendarioMateria.setFechaInicio(calMatDto.getFechaInicio());
+		calendarioMateria.setFechaFin(calMatDto.getFechaFin());
+		calendarioMateria.setMateria(materia);
+		calendarioMateria.setComision(comision);
+
+		return calendarioMateria;
+	}
 
 
 }
