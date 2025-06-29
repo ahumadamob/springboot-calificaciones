7a7a1016dde852094ee2a74614d2c6f072809910
Franco Portal <portalfranco10@gmail.com>
2025-06-29 09:15:26 -0300

    TP02 - Relaciones y DTO implementados (#62)
    Co-authored-by: unknown <kianzo.web@gmail.com>

--- src/main/java/com/imb2025/calificaciones/service/ICursadaService.java
+++ src/main/java/com/imb2025/calificaciones/service/ICursadaService.java
@@ -2,15 +2,17 @@ package com.imb2025.calificaciones.service;
 
 import java.util.List;
 
-
+import com.imb2025.calificaciones.dto.CursadaRequestDTO;
 import com.imb2025.calificaciones.entity.Cursada;
 
 public interface ICursadaService {
-	
-	public List<Cursada> findAll();
-	public Cursada findById(Long id);
-	public Cursada save(Cursada cursada);
-	public void deleteById (Long id);
-	
+
+    public List<Cursada> findAll();
+    Cursada findById(Long id);
+    String save(CursadaRequestDTO cursada);
+    public void deleteById (Long id);
+    public String update(Long id, CursadaRequestDTO dto);
+
 
 }
+
