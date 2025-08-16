ce9f5c5c7d461686b6137a23f34f2d5e630b985f
Mario Ahumada <ahumadamob@gmail.com>
2025-08-16 15:29:00 -0300

    Actualizar RegistroClase a relaciones ManyToOne (#107)

--- src/main/java/com/imb2025/calificaciones/entity/RegistroClase.java
+++ src/main/java/com/imb2025/calificaciones/entity/RegistroClase.java
@@ -2,13 +2,12 @@ package com.imb2025.calificaciones.entity;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class RegistroClase {
@@ -17,23 +16,41 @@ public class RegistroClase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "fecha_clase", nullable = false)
     private LocalDate fecha;
 
-    @Column(name = "tema_materia", nullable = false)
     private String tema;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "docente_id", nullable = false)
     private Docente docente;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "comision_id", nullable = false)
     private Comision comision;
+    
+    public RegistroClase() {
+    }
 
-    // Getters y Setters
+    public RegistroClase(Long id, LocalDate fecha, String tema, Docente docente, Comision comision) {
+        this.id = id;
+        this.fecha = fecha;
+        this.tema = tema;
+        this.docente = docente;
+        this.comision = comision;
+    }
+
+    public RegistroClase(LocalDate fecha, String tema, Docente docente, Comision comision) {
+        this.fecha = fecha;
+        this.tema = tema;
+        this.docente = docente;
+        this.comision = comision;
+    }
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public LocalDate getFecha() {
