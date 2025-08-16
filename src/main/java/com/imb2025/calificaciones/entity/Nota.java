9ff199d698550cc6435b84c606101bdb5ff0e707
Mario Ahumada <ahumadamob@gmail.com>
2025-08-16 15:46:53 -0300

    Replace wildcard imports in entities (#118)

--- src/main/java/com/imb2025/calificaciones/entity/Nota.java
+++ src/main/java/com/imb2025/calificaciones/entity/Nota.java
@@ -1,6 +1,10 @@
 package com.imb2025.calificaciones.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Nota {
