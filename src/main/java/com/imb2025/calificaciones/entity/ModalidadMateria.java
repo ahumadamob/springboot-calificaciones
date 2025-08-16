e46aeec8cd24125e811170a32ea8e84572703a25
Mario Ahumada <ahumadamob@gmail.com>
2025-08-16 15:37:37 -0300

    Añadir entidad ModalidadMateria (#117)

---
+++ src/main/java/com/imb2025/calificaciones/entity/ModalidadMateria.java
@@ -0,0 +1,44 @@
+package com.imb2025.calificaciones.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ModalidadMateria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    public ModalidadMateria() {
+    }
+
+    public ModalidadMateria(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public ModalidadMateria(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
+
