bae5654e434f642de5c355b07a80e9b6e52c29b4
Mario Ahumada <ahumadamob@gmail.com>
2025-08-16 15:46:15 -0300

    Replace wildcard imports in entities

--- src/main/java/com/imb2025/calificaciones/entity/NivelMateria.java
+++ src/main/java/com/imb2025/calificaciones/entity/NivelMateria.java
@@ -1,6 +1,9 @@
 package com.imb2025.calificaciones.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class NivelMateria {
