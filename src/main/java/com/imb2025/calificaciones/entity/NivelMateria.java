862953745f2b2cae7728f4b8d51c9e9fb47c6986
Mario Ahumada <ahumadamob@gmail.com>
2025-08-16 15:23:24 -0300

    Add constructors to NivelMateria entity (#103)

--- src/main/java/com/imb2025/calificaciones/entity/NivelMateria.java
+++ src/main/java/com/imb2025/calificaciones/entity/NivelMateria.java
@@ -1,36 +1,53 @@
 package com.imb2025.calificaciones.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class NivelMateria {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String nombre;
-	private String descripcion;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	
+    private Long id;
 
+    private String nombre;
+
+    private String descripcion;
+
+    public NivelMateria() {
+    }
+
+    public NivelMateria(Long id, String nombre, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public NivelMateria(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
