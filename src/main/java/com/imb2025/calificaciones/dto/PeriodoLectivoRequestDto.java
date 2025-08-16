c14f8a99909a8f823aecbabf24e309915a5eefa6
Alejo Quevedo <carpetaestudiantefabianalejo@proton.me>
2025-08-16 17:09:01 +0000

    TP03 - Quevedo Alejo (#52)
    * TP03 : HTTP y errores
    
    * Correcion en service - delete excepcion
    
    ---------
    
    Co-authored-by: Mario Ahumada <ahumadamob@gmail.com>

--- src/main/java/com/imb2025/calificaciones/dto/PeriodoLectivoRequestDto.java
+++ src/main/java/com/imb2025/calificaciones/dto/PeriodoLectivoRequestDto.java
@@ -2,45 +2,41 @@ package com.imb2025.calificaciones.dto;
 
 import java.time.LocalDate;
 
-public class PeriodoLectivoRequestDto {
-	private Long id;
+public class PeriodoLectivoRequestDTO {
 	private String nombre;
 	private LocalDate fechaInicio;
 	private LocalDate fechaFin;
 	
-	public PeriodoLectivoRequestDto() {}
+	public PeriodoLectivoRequestDTO() {}
 	
-	public PeriodoLectivoRequestDto(Long id, String nombre, LocalDate fechaInicio, LocalDate fechaFin) {
-		this.id = id;
+	public PeriodoLectivoRequestDTO(String nombre, LocalDate fechaInicio, LocalDate fechaFin) {
 		this.nombre = nombre;
 		this.fechaInicio = fechaInicio;
 		this.fechaFin = fechaFin;
 	}
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public LocalDate getFechaInicio() {
 		return fechaInicio;
 	}
+	
 	public void setFechaInicio(LocalDate fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
+	
 	public LocalDate getFechaFin() {
 		return fechaFin;
 	}
+	
 	public void setFechaFin(LocalDate fechaFin) {
 		this.fechaFin = fechaFin;
 	}
-	
 	
 }
--- src/main/java/com/imb2025/calificaciones/service/IPeriodoLectivoService.java
+++ src/main/java/com/imb2025/calificaciones/service/IPeriodoLectivoService.java
@@ -2,14 +2,15 @@ package com.imb2025.calificaciones.service;
 
 import java.util.List;
 
-import com.imb2025.calificaciones.dto.PeriodoLectivoRequestDto;
+import com.imb2025.calificaciones.dto.PeriodoLectivoRequestDTO;
 import com.imb2025.calificaciones.entity.PeriodoLectivo;
 
 public interface IPeriodoLectivoService {
 	List<PeriodoLectivo> findAll();
 	PeriodoLectivo findById(Long id);
+	Boolean existsById(Long id);
 	PeriodoLectivo save(PeriodoLectivo periodoLectivo);
-	PeriodoLectivo update(Long id, PeriodoLectivo periodoLectivo);
-	void deleteById(Long id);
-	PeriodoLectivo RequestDTOToEntity(PeriodoLectivoRequestDto requestDTO);
+	PeriodoLectivo update(Long id, PeriodoLectivo periodoLectivo) throws Exception ;
+	void deleteById(Long id) throws Exception ;
+	PeriodoLectivo mapFromDTO(PeriodoLectivoRequestDTO requestDTO);
 }
