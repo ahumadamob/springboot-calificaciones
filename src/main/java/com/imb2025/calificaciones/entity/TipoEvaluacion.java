9f60d828dd02c65c356a819989e64756dbcb48bb
Mario Ahumada <ahumadamob@gmail.com>
2025-08-16 15:31:25 -0300

    Add constructors for TipoEvaluacion (#110)

--- src/main/java/com/imb2025/calificaciones/entity/TipoEvaluacion.java
+++ src/main/java/com/imb2025/calificaciones/entity/TipoEvaluacion.java
@@ -7,30 +7,43 @@ import jakarta.persistence.Id;
 
 @Entity
 public class TipoEvaluacion {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
-	private String nombre;
-	private String descripcion;
-	
-	//Getters and setters
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}	
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+        private String nombre;
+        private String descripcion;
+
+        public TipoEvaluacion() {
+        }
+
+        public TipoEvaluacion(long id, String nombre, String descripcion) {
+                this.id = id;
+                this.nombre = nombre;
+                this.descripcion = descripcion;
+        }
+
+        public TipoEvaluacion(String nombre, String descripcion) {
+                this.nombre = nombre;
+                this.descripcion = descripcion;
+        }
+
+        public long getId() {
+                return id;
+        }
+        public void setId(long id) {
+                this.id = id;
+        }
+        public String getNombre() {
+                return nombre;
+        }
+        public void setNombre(String nombre) {
+                this.nombre = nombre;
+        }
+        public String getDescripcion() {
+                return descripcion;
+        }
+        public void setDescripcion(String descripcion) {
+                this.descripcion = descripcion;
+        }
 }
