0b63ffc3faeba17b47ae1ac9933ad9d38f36a4be
Alejo Quevedo <carpetaestudiantefabianalejo@proton.me>
2025-08-30 18:16:34 +0000

    Integrar excepciones y DTO genéricos en Periodo Lectivo (#136)

--- src/main/java/com/imb2025/calificaciones/service/jpa/PeriodoLectivoServiceImpl.java
+++ src/main/java/com/imb2025/calificaciones/service/jpa/PeriodoLectivoServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.imb2025.calificaciones.dto.PeriodoLectivoRequestDto;
 import com.imb2025.calificaciones.entity.PeriodoLectivo;
+import com.imb2025.calificaciones.exception.ResourceNotFoundException;
 import com.imb2025.calificaciones.repository.PeriodoLectivoRepository;
 import com.imb2025.calificaciones.service.IPeriodoLectivoService;
 
@@ -23,7 +24,8 @@ public class PeriodoLectivoServiceImpl implements IPeriodoLectivoService{
 
     @Override
     public PeriodoLectivo findById(Long id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+                "Entidad no encontrada con id " + id));
     }
 
     @Override
@@ -32,24 +34,24 @@ public class PeriodoLectivoServiceImpl implements IPeriodoLectivoService{
     }
 
     @Override
-    public PeriodoLectivo update(PeriodoLectivo periodoLectivo, Long id) throws Exception {
-        repository.findById(id).orElseThrow(
-                () -> new Exception("Periodo no encontrado")
-        );
+    public PeriodoLectivo update(PeriodoLectivo periodoLectivo, Long id) {
+    	if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Entidad no encontrada con id " + id);
+        }
         periodoLectivo.setId(id);
         return repository.save(periodoLectivo);
     }
 
     @Override
-    public void deleteById(Long id) throws Exception {
+    public void deleteById(Long id) {
         if (!repository.existsById(id)) {
-            throw new Exception("No se puede eliminar el id: " + id + " porque no existe");
+            throw new ResourceNotFoundException("Entidad no encontrada con id " + id);
         }
         repository.deleteById(id);
     }
 
     @Override
-    public PeriodoLectivo fromDto(PeriodoLectivoRequestDto requestDTO) throws Exception {
+    public PeriodoLectivo fromDto(PeriodoLectivoRequestDto requestDTO) {
         PeriodoLectivo periodoLectivo = new PeriodoLectivo();
 
         periodoLectivo.setNombre(requestDTO.getNombre());
