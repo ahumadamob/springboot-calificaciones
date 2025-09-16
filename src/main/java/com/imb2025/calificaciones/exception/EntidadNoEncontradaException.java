e1ba69fb5bd558894e9c18e0a132e8b57479d17e
Susana Viertel Corona <olgaviertel@gmail.com>
2025-09-16 16:09:26 -0300

    TP04: unificar EntidadNoEncontradaException con ResourceNotFoundException

--- src/main/java/com/imb2025/calificaciones/exception/EntidadNoEncontradaException.java
+++ src/main/java/com/imb2025/calificaciones/exception/EntidadNoEncontradaException.java
@@ -1,12 +1,9 @@
 package com.imb2025.calificaciones.exception;
 
-public class EntidadNoEncontradaException extends RuntimeException {
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+public class EntidadNoEncontradaException extends ResourceNotFoundException {
+    private static final long serialVersionUID = 1L;
 
-	public EntidadNoEncontradaException(String mensaje) {
+    public EntidadNoEncontradaException(String mensaje) {
         super(mensaje);
     }
 }
