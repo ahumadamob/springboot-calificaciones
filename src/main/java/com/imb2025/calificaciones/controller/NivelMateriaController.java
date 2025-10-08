4ecd49e1c6537ebeb96d1ef87f8395a27b7cc4f6
Mario Ahumada <ahumadamob@gmail.com>
2025-10-08 20:49:04 -0300

    Rehacer el commit sin application.properties

--- src/main/java/com/imb2025/calificaciones/controller/NivelMateriaController.java
+++ src/main/java/com/imb2025/calificaciones/controller/NivelMateriaController.java
@@ -1,54 +1,84 @@
 package com.imb2025.calificaciones.controller;
+import java.util.List;
 
-
+import com.imb2025.calificaciones.dto.ApiResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import jakarta.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.imb2025.calificaciones.entity.NivelMateria;
 import com.imb2025.calificaciones.service.INivelMateriaService;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+import com.imb2025.calificaciones.dto.NivelMateriaRequestDto;
+
 
 @RestController
 public class NivelMateriaController {
-	
-	 @Autowired
-	    private INivelMateriaService nivelMateriaService;
-	 
-	 @GetMapping("/api/nivelmateria")
-	 public List<NivelMateria> getAllNivelMateria() {
-	        return nivelMateriaService.findAll();
-	 }
-	 
-	 @GetMapping("/api/nivelmateria/{id}")
-	 public NivelMateria getNivelMateriaById(@PathVariable("id")Long id) {
-	        return nivelMateriaService.findById(id);  
-	 }       
-        @PostMapping("/api/nivelmateria")
-        public NivelMateria createNivelMateria(@RequestBody NivelMateria nivelMateria){
-                return nivelMateriaService.create(nivelMateria);
-        }
-        @PutMapping("/api/nivelmateria")
-         public NivelMateria updateNivelMateria(@RequestBody NivelMateria nivelMateria){
-                 try {
-                        return nivelMateriaService.update(nivelMateria, nivelMateria.getId());
-                 } catch (Exception e) {
-                        return null;
-                 }
-         }
-         @DeleteMapping("/api/nivelmateria/{id}")
-         public void deleteNivelMateria(@PathVariable("id")Long id) {
-                 try {
-                        nivelMateriaService.deleteById(id);
-                 } catch (Exception e) {
-                        // manejar
-                 }
-         }
+@Autowired
+private INivelMateriaService nivelMateriaService;
+
+@GetMapping("/api/nivelmateria")
+public ResponseEntity<ApiResponseDTO<List<NivelMateria>>> getAllNivelMateria() {
+    List<NivelMateria> nivelMaterias = nivelMateriaService.findAll();
+    ApiResponseDTO<List<NivelMateria>> response = new ApiResponseDTO<>();
+    response.setSuccess(true);
+    response.setData(nivelMaterias);
+    response.setMessage("Lista de NivelMateria");
+    return ResponseEntity.ok(response);
+}
+@PostMapping("/api/nivelmateria")
+public ResponseEntity<ApiResponseDTO<NivelMateria>> createNivelMateria(@RequestBody @Valid NivelMateriaRequestDto nivelDto) {
+    NivelMateria nivel = new NivelMateria();
+    nivel.setNombre(nivelDto.getNombre());
+    nivel.setDescripcion(nivelDto.getDescripcion());
+
+    NivelMateria created = nivelMateriaService.create(nivel);
+
+    ApiResponseDTO<NivelMateria> response = new ApiResponseDTO<>();
+    response.setSuccess(true);
+    response.setData(created);
+    response.setMessage("NivelMateria creada exitosamente");
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+}
+
+@PutMapping("/api/nivelmateria/{id}")
+public ResponseEntity<ApiResponseDTO<NivelMateria>> updateNivelMateria(@PathVariable Long id, @RequestBody @Valid NivelMateriaRequestDto nivelDto) {
+    try {
+        NivelMateria nivel = nivelMateriaService.fromDto(nivelDto);
+        NivelMateria updated = nivelMateriaService.update(nivel, id);
+        ApiResponseDTO<NivelMateria> response = new ApiResponseDTO<NivelMateria>(true, updated, "NivelMateria actualizada exitosamente");
+        return ResponseEntity.ok(response);
+    } catch (Exception e) {
+        ApiResponseDTO<NivelMateria> response = new ApiResponseDTO<NivelMateria>();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
+
+@DeleteMapping("/api/nivelmateria/{id}")
+public ResponseEntity<ApiResponseDTO<Void>> deleteNivelMateria(@PathVariable Long id) {
+    try {
+        nivelMateriaService.deleteById(id);
+        ApiResponseDTO<Void> response = new ApiResponseDTO<>();
+        response.setSuccess(true);
+        response.setMessage("NivelMateria eliminada correctamente");
+        return ResponseEntity.ok(response);
+    } catch (Exception e) {
+        ApiResponseDTO<Void> response = new ApiResponseDTO<>();
+        response.setSuccess(false);
+        response.setMessage("Error al eliminar: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+}
 }
