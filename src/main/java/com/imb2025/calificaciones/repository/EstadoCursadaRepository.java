8959a3bcb10862d711a2b6acf3d4d20378747882
Francisco Vargas <83306085+franv35@users.noreply.github.com>
2025-10-08 21:08:55 -0300

    tp07-vargas-francisco (#187)

--- src/main/java/com/imb2025/calificaciones/repository/EstadoCursadaRepository.java
+++ src/main/java/com/imb2025/calificaciones/repository/EstadoCursadaRepository.java
@@ -1,11 +1,15 @@
 package com.imb2025.calificaciones.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import com.imb2025.calificaciones.entity.EstadoCursada;
 
 @Repository
 public interface EstadoCursadaRepository extends JpaRepository<EstadoCursada, Long> {
+	List<EstadoCursada> findByNombreIgnoreCase(String nombre);
+	
+	long countByDescripcionIgnoreCase(String descripcion);
 }
 
