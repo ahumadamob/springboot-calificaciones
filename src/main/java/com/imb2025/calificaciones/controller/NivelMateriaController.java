46d2ca4f7e6194c41e88c3dc5b3bcf8119e940d2
Mario Ahumada <ahumadamob@gmail.com>
2025-10-08 21:20:51 -0300

    Correcciones del profesor

--- src/main/java/com/imb2025/calificaciones/controller/NivelMateriaController.java
+++ src/main/java/com/imb2025/calificaciones/controller/NivelMateriaController.java
@@ -1,7 +1,6 @@
 package com.imb2025.calificaciones.controller;
 import java.util.List;
 
-import com.imb2025.calificaciones.dto.ApiResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +18,7 @@ import com.imb2025.calificaciones.entity.NivelMateria;
 import com.imb2025.calificaciones.service.INivelMateriaService;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+import com.imb2025.calificaciones.dto.ApiResponseSuccessDto;
 import com.imb2025.calificaciones.dto.NivelMateriaRequestDto;
 
 
@@ -29,23 +28,23 @@ public class NivelMateriaController {
 private INivelMateriaService nivelMateriaService;
 
 @GetMapping("/api/nivelmateria")
-public ResponseEntity<ApiResponseDTO<List<NivelMateria>>> getAllNivelMateria() {
+public ResponseEntity<ApiResponseSuccessDto<List<NivelMateria>>> getAllNivelMateria() {
     List<NivelMateria> nivelMaterias = nivelMateriaService.findAll();
-    ApiResponseDTO<List<NivelMateria>> response = new ApiResponseDTO<>();
+    ApiResponseSuccessDto<List<NivelMateria>> response = new ApiResponseSuccessDto<>();
     response.setSuccess(true);
     response.setData(nivelMaterias);
     response.setMessage("Lista de NivelMateria");
     return ResponseEntity.ok(response);
 }
 @PostMapping("/api/nivelmateria")
-public ResponseEntity<ApiResponseDTO<NivelMateria>> createNivelMateria(@RequestBody @Valid NivelMateriaRequestDto nivelDto) {
+public ResponseEntity<ApiResponseSuccessDto<NivelMateria>> createNivelMateria(@RequestBody @Valid NivelMateriaRequestDto nivelDto) {
     NivelMateria nivel = new NivelMateria();
     nivel.setNombre(nivelDto.getNombre());
     nivel.setDescripcion(nivelDto.getDescripcion());
 
     NivelMateria created = nivelMateriaService.create(nivel);
 
-    ApiResponseDTO<NivelMateria> response = new ApiResponseDTO<>();
+    ApiResponseSuccessDto<NivelMateria> response = new ApiResponseSuccessDto<>();
     response.setSuccess(true);
     response.setData(created);
     response.setMessage("NivelMateria creada exitosamente");
@@ -54,28 +53,28 @@ public ResponseEntity<ApiResponseDTO<NivelMateria>> createNivelMateria(@RequestB
 }
 
 @PutMapping("/api/nivelmateria/{id}")
-public ResponseEntity<ApiResponseDTO<NivelMateria>> updateNivelMateria(@PathVariable Long id, @RequestBody @Valid NivelMateriaRequestDto nivelDto) {
+public ResponseEntity<ApiResponseSuccessDto<NivelMateria>> updateNivelMateria(@PathVariable Long id, @RequestBody @Valid NivelMateriaRequestDto nivelDto) {
     try {
         NivelMateria nivel = nivelMateriaService.fromDto(nivelDto);
         NivelMateria updated = nivelMateriaService.update(nivel, id);
-        ApiResponseDTO<NivelMateria> response = new ApiResponseDTO<NivelMateria>(true, updated, "NivelMateria actualizada exitosamente");
+        ApiResponseSuccessDto<NivelMateria> response = new ApiResponseSuccessDto<NivelMateria>(true, "NivelMateria actualizada exitosamente", updated);
         return ResponseEntity.ok(response);
     } catch (Exception e) {
-        ApiResponseDTO<NivelMateria> response = new ApiResponseDTO<NivelMateria>();
+    	ApiResponseSuccessDto<NivelMateria> response = new ApiResponseSuccessDto<NivelMateria>();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
 
 @DeleteMapping("/api/nivelmateria/{id}")
-public ResponseEntity<ApiResponseDTO<Void>> deleteNivelMateria(@PathVariable Long id) {
+public ResponseEntity<ApiResponseSuccessDto<Void>> deleteNivelMateria(@PathVariable Long id) {
     try {
         nivelMateriaService.deleteById(id);
-        ApiResponseDTO<Void> response = new ApiResponseDTO<>();
+        ApiResponseSuccessDto<Void> response = new ApiResponseSuccessDto<>();
         response.setSuccess(true);
         response.setMessage("NivelMateria eliminada correctamente");
         return ResponseEntity.ok(response);
     } catch (Exception e) {
-        ApiResponseDTO<Void> response = new ApiResponseDTO<>();
+    	ApiResponseSuccessDto<Void> response = new ApiResponseSuccessDto<>();
         response.setSuccess(false);
         response.setMessage("Error al eliminar: " + e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
