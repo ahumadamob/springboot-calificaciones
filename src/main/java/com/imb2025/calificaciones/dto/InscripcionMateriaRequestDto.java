2a24dd0943c352f9811075cd135ebcc9cf1a02c3
Victor Nievas <victornievaszxz@gmail.com>
2025-10-12 17:13:52 -0300

    Validaciones JSR-380 en InscripcionMateriaRequestDto

--- src/main/java/com/imb2025/calificaciones/dto/InscripcionMateriaRequestDto.java
+++ src/main/java/com/imb2025/calificaciones/dto/InscripcionMateriaRequestDto.java
@@ -1,9 +1,17 @@
 package com.imb2025.calificaciones.dto;
 
-public class InscripcionMateriaRequestDto {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
+public class InscripcionMateriaRequestDto {
+    @NotNull(message ="El id del alumno no puede estar vacío")
+    @Min(1)
     private Long alumnoId;
+    @NotNull(message = "El id de la materia no puede estar vacío")
+    @Min(1)
     private Long materiaId;
+    @NotNull(message = "el id del periodo electivo no puede estar vacío")
+    @Min(1)
     private Long periodoLectivoId;
 
     public Long getAlumnoId() {
