d37a5d168a6890c64968b83c1241d9de63274979
Alejo Quevedo <carpetaestudiantefabianalejo@proton.me>
2025-10-14 18:49:03 +0000

    TP08 - Alejo Quevedo - Refactor de DTOs y Mapper (#196)
    * TP08 - Refactor de DTOs y Mapper
    
    * Añadido PeriodoLectivoReponseDto.java
    Se olvido enviarlo en el anterior commit
    
    * PeriodoLectivoMapper añadido
    Arreglado el error del git donde no se incluyo en el primer commit.

--- src/main/java/com/imb2025/calificaciones/dto/response/PeriodoLectivoResponseDto.java
+++ src/main/java/com/imb2025/calificaciones/dto/response/PeriodoLectivoResponseDto.java
@@ -1,45 +1,63 @@
-package com.imb2025.calificaciones.entity;
+package com.imb2025.calificaciones.dto.response;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-
-@Entity
-public class PeriodoLectivo extends BaseEntity {
+public class PeriodoLectivoResponseDto {
+	private Long id;
 	private String nombre;
 	private LocalDate fechaInicio;
 	private LocalDate fechaFin;
-
-	public PeriodoLectivo() {}
-
-	public PeriodoLectivo(String nombre, LocalDate fechaInicio, LocalDate fechaFin) {
+    private Long version;
+    
+    public PeriodoLectivoResponseDto() {
+    }
+    
+	public PeriodoLectivoResponseDto(Long id, String nombre, LocalDate fechaInicio, LocalDate fechaFin, Long version) {
+		this.id = id;
 		this.nombre = nombre;
 		this.fechaInicio = fechaInicio;
 		this.fechaFin = fechaFin;
+		this.version = version;
 	}
-
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getNombre() {
 		return nombre;
 	}
-
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	
 	public LocalDate getFechaInicio() {
 		return fechaInicio;
 	}
-
+	
 	public void setFechaInicio(LocalDate fechaInicio) {
 		this.fechaInicio = fechaInicio;
 	}
-
+	
 	public LocalDate getFechaFin() {
 		return fechaFin;
 	}
-
+	
 	public void setFechaFin(LocalDate fechaFin) {
 		this.fechaFin = fechaFin;
 	}
 	
+	public Long getVersion() {
+		return version;
+	}
+	
+	public void setVersion(Long version) {
+		this.version = version;
+	}
+    
 }
