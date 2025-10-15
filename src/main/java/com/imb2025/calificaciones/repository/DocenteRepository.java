3b2e581fff5dcf7479cd383f8ce583991b5464ba
Andres <kianzo.web@gmail.com>
2025-10-15 20:20:04 -0300

    TP07 - Métodos mágicos en Repository

--- src/main/java/com/imb2025/calificaciones/repository/DocenteRepository.java
+++ src/main/java/com/imb2025/calificaciones/repository/DocenteRepository.java
@@ -1,9 +1,16 @@
 package com.imb2025.calificaciones.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.imb2025.calificaciones.entity.Docente;
 
+@Repository
 public interface DocenteRepository extends JpaRepository<Docente, Long> {
-}
 
+    List<Docente> findByApellido(String apellido);
+
+    Long countByTitulo(String titulo);
+}
