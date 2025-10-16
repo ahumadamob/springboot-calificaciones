06feae15722fb74339e9b7a4ca32992958ced68a
Victor Nievas <victornievaszxz@gmail.com>
2025-10-16 13:46:26 -0300

    Metodos Magicos en InscripcionMateriaRepository

--- src/main/java/com/imb2025/calificaciones/repository/InscripcionMateriaRepository.java
+++ src/main/java/com/imb2025/calificaciones/repository/InscripcionMateriaRepository.java
@@ -1,5 +1,7 @@
 package com.imb2025.calificaciones.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.imb2025.calificaciones.entity.InscripcionMateria;
 
 @Repository
 public interface InscripcionMateriaRepository extends JpaRepository<InscripcionMateria, Long> {
+    public List<InscripcionMateria> findByAlumno_Id(Long idAlumno);
+    public long countByAlumno_Id(Long idAlumno);
 }
 
