4c77afe2aaceb4e2a542924fe82fd86966c490ad
Tomás Sosa <tsosa416@gmail.com>
2025-10-16 16:15:14 -0300

    Refactor: CondicionFinal extiende BaseEntity

--- src/main/java/com/imb2025/calificaciones/entity/CondicionFinal.java
+++ src/main/java/com/imb2025/calificaciones/entity/CondicionFinal.java
@@ -1,40 +1,16 @@
 package com.imb2025.calificaciones.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
 
 @Entity
-public class CondicionFinal {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class CondicionFinal extends BaseEntity {
 
     private String nombre;
 
-    @Version
-    private Long version; // ✅ este campo faltaba
-
     public CondicionFinal() {}
-
-    public CondicionFinal(Long id, String nombre) {
-        this.id = id;
-        this.nombre = nombre;
-    }
 
     public CondicionFinal(String nombre) {
         this.nombre = nombre;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getNombre() {
@@ -43,13 +19,5 @@ public class CondicionFinal {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
     }
 }
