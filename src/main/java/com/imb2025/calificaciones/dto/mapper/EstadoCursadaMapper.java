fe8bcd49371574063dd1ac42edf4eb492ebbc38e
franv35 <vargas.franemi@gmail.com>
2025-10-16 23:03:30 -0300

    Corrección TP08

---
+++ src/main/java/com/imb2025/calificaciones/dto/mapper/EstadoCursadaMapper.java
@@ -0,0 +1,24 @@
+package com.imb2025.calificaciones.dto.mapper;
+
+import com.imb2025.calificaciones.dto.request.EstadoCursadaRequestDto;
+import com.imb2025.calificaciones.dto.response.EstadoCursadaResponseDto;
+import com.imb2025.calificaciones.entity.EstadoCursada;
+
+public class EstadoCursadaMapper {
+
+    public EstadoCursada fromDto(EstadoCursadaRequestDto estadoCursadaRequestDto) throws Exception {
+        EstadoCursada estadoCursada = new EstadoCursada();
+        estadoCursada.setNombre(estadoCursadaRequestDto.getNombre());
+        estadoCursada.setDescripcion(estadoCursadaRequestDto.getDescripcion());
+        return estadoCursada;
+    }
+    
+    public EstadoCursadaResponseDto toResponseDto(EstadoCursada estadoCursada) {
+    	EstadoCursadaResponseDto dto = new EstadoCursadaResponseDto();
+    	dto.setId(estadoCursada.getId());
+    	dto.setNombre(estadoCursada.getNombre());
+    	dto.setDescipcion(estadoCursada.getDescripcion());
+    	
+    	return dto;
+    }
+}
---
+++ src/main/java/com/imb2025/calificaciones/dto/response/EstadoCursadaResponseDto.java
@@ -0,0 +1,28 @@
+package com.imb2025.calificaciones.dto.response;
+
+public class EstadoCursadaResponseDto {
+	private long id;
+	private String nombre;
+	private String descipcion;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescipcion() {
+		return descipcion;
+	}
+	public void setDescipcion(String descipcion) {
+		this.descipcion = descipcion;
+	}
+	
+	
+}
