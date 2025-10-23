8a7ae70381e6567379ab2a2372d7459088e1c047
AgusMarconi <agustinamarconi39@gmail.com>
2025-10-23 07:47:16 -0300

    TP07: metodos magicos Evaluacion

--- src/main/java/com/imb2025/calificaciones/service/IEvaluacionService.java
+++ src/main/java/com/imb2025/calificaciones/service/IEvaluacionService.java
@@ -2,6 +2,8 @@ package com.imb2025.calificaciones.service;
 
 import com.imb2025.calificaciones.dto.EvaluacionRequestDto;
 import com.imb2025.calificaciones.entity.Evaluacion;
+
+import java.util.Date;
 import java.util.List;
 
 public interface IEvaluacionService {
@@ -17,4 +19,9 @@ public interface IEvaluacionService {
     public void deleteById(Long id) throws Exception;
 
     public Evaluacion fromDto(EvaluacionRequestDto dto) throws Exception;
+    
+    public List<Evaluacion> findByMateriaIdAndComisionId(long materiaId, long comisionId);
+	public long countByMateriaIdAndComisionId(long materiaId, long comisionId);
+	public List<Evaluacion> findByFechaBetween(Date fechaInicio, Date fechaFin);
+
 }
