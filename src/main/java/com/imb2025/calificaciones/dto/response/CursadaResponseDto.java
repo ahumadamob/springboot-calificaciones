3f087f76232d695385467016b3c5c3d65351a1b7
Franco Portal <portalfranco10@gmail.com>
2025-11-05 09:11:59 -0300

    Refactor de DTOs y Mapper para Cursada (#247)
    * Refactor de DTOs y Mapper para Cursada
    
    * Se incluyo el archivo Entity, extendiendo desde BaseEntity
    
    * Correccion constructor undefined
    
    ---------
    
    Co-authored-by: unknown <kianzo.web@gmail.com>

---
+++ src/main/java/com/imb2025/calificaciones/dto/response/CursadaResponseDto.java
@@ -0,0 +1,41 @@
+package com.imb2025.calificaciones.dto.response;
+
+public class CursadaResponseDto {
+	    private Long id;
+	    private Long alumnoId;
+	    private Long materiaId;
+	    private Long anioLectivoId;
+	    private Long condicionFinalId;
+	    private Integer version;
+
+	    public CursadaResponseDto() {
+	    }
+
+	    public CursadaResponseDto(Long id, Long alumnoId, Long materiaId, Long anioLectivoId, Long condicionFinalId, Integer version) {
+	        this.id = id;
+	        this.alumnoId = alumnoId;
+	        this.materiaId = materiaId;
+	        this.anioLectivoId = anioLectivoId;
+	        this.condicionFinalId = condicionFinalId;
+	        this.version = version;
+	    }
+
+	    public Long getId() { return id; }
+	    public void setId(Long id) { this.id = id; }
+
+	    public Long getAlumnoId() { return alumnoId; }
+	    public void setAlumnoId(Long alumnoId) { this.alumnoId = alumnoId; }
+
+	    public Long getMateriaId() { return materiaId; }
+	    public void setMateriaId(Long materiaId) { this.materiaId = materiaId; }
+
+	    public Long getAnioLectivoId() { return anioLectivoId; }
+	    public void setAnioLectivoId(Long anioLectivoId) { this.anioLectivoId = anioLectivoId; }
+
+	    public Long getCondicionFinalId() { return condicionFinalId; }
+	    public void setCondicionFinalId(Long condicionFinalId) { this.condicionFinalId = condicionFinalId; }
+
+	    public Integer getVersion() { return version; }
+	    public void setVersion(Integer version) { this.version = version; }
+	}
+
--- src/main/java/com/imb2025/calificaciones/entity/Cursada.java
+++ src/main/java/com/imb2025/calificaciones/entity/Cursada.java
@@ -1,18 +1,11 @@
 package com.imb2025.calificaciones.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
 @Entity
-public class Cursada {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Cursada extends BaseEntity {
 
     @ManyToOne
     @JoinColumn(name = "alumno_id")
@@ -33,21 +26,14 @@ public class Cursada {
     public Cursada() {
     }
 
-    public Cursada(Long id, Alumno alumno, Materia materia, AnioLectivo anioLectivo, CondicionFinal condicionFinal) {
-        this.id = id;
+    public Cursada(Alumno alumno, Materia materia, AnioLectivo anioLectivo, CondicionFinal condicionFinal) {
+        
         this.alumno = alumno;
         this.materia = materia;
         this.anioLectivo = anioLectivo;
         this.condicionFinal = condicionFinal;
     }
     
-    public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public Alumno getAlumno() {
         return alumno;
