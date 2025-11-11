f82204823cdf886c23514ac25dad44f878f053de
GitPablosvm <pablosvm17@gmail.com>
2025-11-11 00:22:10 -0300

    TP08: Service sin DTOs

--- src/main/java/com/imb2025/calificaciones/repository/TipoEvaluacionRepository.java
+++ src/main/java/com/imb2025/calificaciones/repository/TipoEvaluacionRepository.java
@@ -3,8 +3,6 @@ package com.imb2025.calificaciones.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.imb2025.calificaciones.entity.TipoEvaluacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.imb2025.calificaciones.entity.TipoEvaluacion;
 
 public interface TipoEvaluacionRepository extends JpaRepository<TipoEvaluacion, Long> {
 
