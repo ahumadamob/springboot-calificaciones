6eb2612ee37210886329de0392de67f6032aa8b9
Mario Ahumada <ahumadamob@gmail.com>
2025-11-16 16:24:52 -0300

    Adjust tipo evaluacion endpoints and mvnw permissions

--- src/main/java/com/imb2025/calificaciones/controller/TipoEvaluacionController.java
+++ src/main/java/com/imb2025/calificaciones/controller/TipoEvaluacionController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -69,8 +68,8 @@ public class TipoEvaluacionController {
         return ResponseEntity.ok(new ApiResponseSuccessDto<>(true, "Tipo de Evaluacion eliminada correctamente", null));
     }
 
-    @GetMapping("/buscar")
-    public ResponseEntity<ApiResponseSuccessDto<List<TipoEvaluacion>>> search(@RequestParam String q) {
+    @GetMapping("/buscar/{q}")
+    public ResponseEntity<ApiResponseSuccessDto<List<TipoEvaluacion>>> search(@PathVariable String q) {
 
         var lista = tipoEvaluacionService.buscarNombre(q);
 
@@ -78,8 +77,8 @@ public class TipoEvaluacionController {
         return ResponseEntity.ok(new ApiResponseSuccessDto<>(true, "se realizo la busqueda correctamente", lista));
     }
 
-    @GetMapping("/contar")
-    public ResponseEntity<ApiResponseSuccessDto<Long>> count(@RequestParam String q) {
+    @GetMapping("/contar/{q}")
+    public ResponseEntity<ApiResponseSuccessDto<Long>> count(@PathVariable String q) {
 
         long total = tipoEvaluacionService.contarNombre(q);
         return ResponseEntity.ok(new ApiResponseSuccessDto<>(true, "Total: ", total));
